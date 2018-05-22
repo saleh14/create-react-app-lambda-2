48b705cd451b312990db110b302de4977b8e1eda
Saleh Almera <saleh.mearaj@gmail.com>
2018-05-22 12:26:56 +0300

    sending requires login

--- src/App.js
+++ src/App.js
@@ -1,43 +1,67 @@
 import React, { Component } from 'react'
+import netlifyIdentity from 'netlify-identity-widget'
 import './App.css'
+
 class SlackMessage extends Component {
   constructor (props) {
     super(props)
     this.state = { loading: false, text: null, error: null, success: false }
   }
+
+  generateHeaders () {
+    const headers = { 'Content-Type': 'application/json' }
+    if (netlifyIdentity.currentUser()) {
+      return netlifyIdentity.currentUser().jwt().then(token => {
+        return { ...headers, Authorization: `Bearer ${token}` }
+      })
+    }
+    return Promise.resolve(headers)
+  }
+
   handleText = e => {
     this.setState({ text: e.target.value })
   }
+
   handleSubmit = e => {
     e.preventDefault()
+
     this.setState({ loading: true })
-    fetch('/.netlify/functions/slack', {
-      method: 'POST',
-      body: JSON.stringify({
-        text: this.state.text
-      })
-    })
-      .then(response => {
-        if (!response.ok) {
-          return response.text().then(err => {
-            throw err
-          })
-        }
-      })
-      .then(() =>
-        this.setState({
-          loading: false,
-          text: null,
-          success: true,
-          error: null
+    this.generateHeaders().then(headers => {
+      fetch('/.netlify/functions/slack', {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({
+          text: this.state.text
         })
-      )
-      .catch(err =>
-        this.setState({ loading: false, success: false, error: err.toString() })
-      )
+      })
+        .then(response => {
+          if (!response.ok) {
+            response.text().then(err => {
+              throw err
+            })
+          }
+        })
+        .then(() =>
+          this.setState({
+            loading: false,
+            text: null,
+            success: true,
+            error: null
+          })
+        )
+        .catch(err =>
+          this.setState({
+            loading: false,
+            success: false,
+            error: err.toString()
+          })
+        )
+    })
   }
+
   render () {
     const { loading, text, error, success } = this.state
+
     return (
       <form onSubmit={this.handleSubmit}>
         {error && <p><strong>Error sending message: {error}</strong></p>}
@@ -57,16 +81,28 @@ class SlackMessage extends Component {
     )
   }
 }
+
 class App extends Component {
+  componentDidMount () {
+    netlifyIdentity.init()
+  }
+
+  handleIdentity = e => {
+    e.preventDefault()
+    netlifyIdentity.open()
+  }
+
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
           <h1 className='App-title'>Slack Messenger</h1>
         </header>
+        <p><a href='#' onClick={this.handleIdentity}>User Status</a></p>
         <SlackMessage />
       </div>
     )
   }
 }
+
 export default App
--- src/lambda/slack.js
+++ src/lambda/slack.js
@@ -1,20 +1,39 @@
-const fetch = require("node-fetch");
-const slackURL = process.env.SLACK_WEBHOOK_URL;
-export function handler(event, context, callback) {
-  if (event.httpMethod !== "POST") {
-    return callback(null, { statusCode: 410, body: "Unsupported Request Method" });
-  }
-  try {
-    const payload = JSON.parse(event.body);
-    fetch(slackURL, {
-      method: "POST",
-      body: JSON.stringify({ text: payload.text })
-    }).then(() => {
-      callback(null, { statusCode: 204 });
-    }).catch((e) => {
-      callback(null, { statusCode: 500, body: "Internal Server Error: " + e });
+const fetch = require('node-fetch')
+const slackURL = process.env.SLACK_WEBHOOK_URL
+
+export function handler (event, context, callback) {
+  if (event.httpMethod !== 'POST') {
+    return callback(null, {
+      statusCode: 410,
+      body: 'Unsupported Request Method'
     })
+  }
+
+  const claims = context.clientContext && context.clientContext.user
+  if (!claims) {
+    return callback(null, {
+      statusCode: 401,
+      body: 'You must be signed in to call this function'
+    })
+  }
+
+  try {
+    const payload = JSON.parse(event.body)
+
+    fetch(slackURL, {
+      method: 'POST',
+      body: JSON.stringify({
+        text: payload.text,
+        attachments: [{ text: `From ${claims.email}` }]
+      })
+    })
+      .then(() => {
+        callback(null, { statusCode: 204 })
+      })
+      .catch(e => {
+        callback(null, { statusCode: 500, body: 'Internal Server Error: ' + e })
+      })
   } catch (e) {
-    callback(null, { statusCode: 500, body: "Internal Server Error: " + e });
+    callback(null, { statusCode: 500, body: 'Internal Server Error: ' + e })
   }
 }
